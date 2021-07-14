33f6e8d3e3ef8757eb7d36ae32a6ad2aa3584e5d
Rithick <rithick0907@gmail.com>
2021-07-14 13:33:42 +0530

    JWT is persisted using localStorage

--- src/App.js
+++ src/App.js
@@ -4,11 +4,17 @@ import UserContext from "./contexts/UserContext";
 import { useState } from "react";
 
 const App = () => {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(() => localStorage.getItem("token"));
 
-  const loginHandler = (token) => setToken(token);
+  const loginHandler = (token) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+  };
 
-  const logoutHandler = () => setToken(null);
+  const logoutHandler = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
 
   return (
     <UserContext.Provider value={{ token, loginHandler, logoutHandler }}>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,13 @@
 import * as Yup from "yup";
 
 import { CustomForm, Input, SubmitButton } from "../components/form";
+import { instance, passwordUpdateURL } from "../services/httpConfig";
 
-import { Link } from "react-router-dom";
 import { StyledLogin } from "./styles";
+import UserContext from "../contexts/UserContext";
 import { passwordValidation } from "../validate";
+import { toast } from "react-toastify";
+import { useContext } from "react";
 
 const initialValues = {
   password: "",
@@ -17,10 +20,37 @@ const validationSchema = Yup.object().shape({
     .label("Password"),
 });
 const Profile = () => {
+  const userContext = useContext(UserContext);
+
   const handleSubmit = async (val, { isSubmitting, resetForm }) => {
-    console.log(val);
-    isSubmitting(false);
-    resetForm();
+    const { password } = val;
+    try {
+      const { data } = await instance({
+        url: passwordUpdateURL,
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: {
+          idToken: userContext.token,
+          password,
+          returnSecureToken: true,
+        },
+      });
+      console.log(data);
+    } catch (e) {
+      let errorMessage = "Something went wrong";
+      if (
+        e.response &&
+        e.response.data &&
+        e.response.data.error &&
+        e.response.data.error.message
+      ) {
+        errorMessage = e.response.data.error.message;
+      }
+      toast.error(errorMessage);
+      resetForm();
+    }
   };
   return (
     <StyledLogin>
